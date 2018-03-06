143b2ffdc29dd75c350603f6785558f008950ee1
Paul Szkwarek <szkwarek@gmail.com>
2018-03-06 14:33:33 -0500

    removed the work on the user_controller page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,43 +2,15 @@ class UsersController < ApplicationController
 
   def show
     authorize @user
-
   end
 
   def index
-    @offers = policy_scope(Offer).order(created_at: :desc)
+
   end
 
-  # def new
-  #   @user = User.new
-  #   authorize @user
-  # end
 
-  # def create
-  #   @user = User.new(user_params)
-  #   @user = current_user
-  #   authorize @user
 
-  #   if @user.save!
-  #     redirect_to users_path
-  #   end
-  # end
 
-  # def destroy
-  #   if @user.matches.empty?
-  #     @user.destroy!
-  #     redirect_to users_path
-  #   else
-  #     redirect_to user_path(@user)
-  #   end
-  # end
-
-  # def update
-  #   @user.update(user_params)
-  #   redirect_to_ user_path(@user)
-  # end
-
-  private
 
 
 end
