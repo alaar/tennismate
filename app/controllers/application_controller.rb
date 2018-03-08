e3988fb197f893d5433f94bc1dcad0133c0a9689
Gina Ko <ginako211@gmail.com>
2018-03-08 12:04:53 -0500

    css for court-show & signin page function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 #   protect_from_forgery with: :exception
 #   before_action :authenticate_user!
 #   include Pundit
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 
   before_action :authenticate_user!, :configure_permitted_paramaters, if: :devise_controller?
 
