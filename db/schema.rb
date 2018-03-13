e9709404ad46675cfa0de8504495377e5b2d147a
Asma Laaribi <laaribi.asm@gmail.com>
2018-03-13 18:24:17 -0400

    improved the edit availabilities functionality

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312145234) do
+ActiveRecord::Schema.define(version: 20180313173235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,14 @@ ActiveRecord::Schema.define(version: 20180312145234) do
     t.datetime "updated_at", null: false
     t.bigint "court_id"
     t.index ["court_id"], name: "index_matches_on_court_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "punctuality"
+    t.integer "sportsmanship"
+    t.integer "accuracy"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
