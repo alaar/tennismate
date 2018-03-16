5342dc6be900ccab540bffe04460e1d1a6b19a74
Asma Laaribi <laaribi.asm@gmail.com>
2018-03-16 12:30:24 -0400

    improved dropdown of the user show page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,5 @@
 import "bootstrap";
+import "./../components/select";
 import {initializeAvailabilities} from "./../components/availabilities_form";
 import { initializeSlickCarousel } from "./../components/user_slider";
 
